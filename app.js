e5aad595f114f518ea20937997479b824acfab9a
Abdullah <abdulluhodulate@gmail.com>
2023-02-25 15:29:16 +0100

    updated nav links

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const hamburger = document.querySelector("#hamburger");
 const mobileMenu = document.querySelector("#mobileMenu");
 const header = document.querySelector("header");
+const ul_links = document.querySelectorAll("#header .nav-list ul a");
 
 hamburger.addEventListener("click", () => {
 	hamburger.classList.toggle("active");
 	mobileMenu.classList.toggle("active");
+});
+ul_links.forEach((links) => {
+	links.addEventListener("click", () => {
+		hamburger.classList.toggle("active");
+		mobileMenu.classList.toggle("active");
+	});
+});
+
+document.addEventListener("scroll", () => {
+	let scrollPosition = window.scrollY;
+	if (scrollPosition > 270) {
+		header.style.backgroundColor = "grey";
+	} else {
+		header.style.backgroundColor = "transparent";
+	}
 });
